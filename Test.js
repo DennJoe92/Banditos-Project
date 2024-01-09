9597246a13cc320a59910e5cdac09afb55a20e23
DennJoe92 <153688190+DennJoe92@users.noreply.github.com>
2024-01-09 15:29:10 +0000

    Javascript learning
    learning javascript fundimentals

---
+++ Test.js
@@ -0,0 +1,14 @@
+//Adding First and last name variables
+let firstName = "Joe";
+let lastName = "Amos";
+
+//submitting variables to the console
+console.log(`Hello ${firstName} ${lastName}`);
+
+/*This is a multi line comment so that 
+*i know you can do it
+*/
+
+let handiCap = "20"
+
+console.log (`Your Handicap is ${handiCap}`);
