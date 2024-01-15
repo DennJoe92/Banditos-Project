9a6d7a94cfeefc8be7788347a436914f85f414b1
DennJoe92 <153688190+DennJoe92@users.noreply.github.com>
2024-01-15 16:59:03 +0000

    Express Setup

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const port = 3000;
 
-// Connect to MongoDB Atlas
-const atlasConnectionString = 'mongodb+srv://joeplamos:<LennyOzzy23%21>@golfsociety.3bgjga1.mongodb.net/';
-mongoose.connect(atlasConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Check MongoDB connection
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB Atlas');
-});
-
-// ...
